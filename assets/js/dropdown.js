ff1f684f7f0c04bc82f152c3fcfaf269931ee110
Joel Lithgow <joelnotlithgow@gmail.com>
2021-06-04 19:07:45 -0400

    chapter 6. ugh

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,14 +1,19 @@
-function dropdown(section_container) {
-  console.log(section_container)
-  let dropdown_container = section_container.querySelector('.dropdown-nav');
-  console.log(dropdown_container)
-  dropdown_container.classList.toggle('hidden');
-
-  /*
-    let dropdown = section_container.children[0];
-    let dropdown_items = section_container.children;
-    for (let x=0; x<dropdown_items.length; x++) {
-      dropdown_items[x].classList.toggle('hidden');
+const addOffClick = (e, cb) => {
+  const offClick = (evt) => {
+    if (e !== evt) {
+      cb();
+      document.removeEventListener('click', offClick);
     }
-  */
+  }
+  document.addEventListener('click', offClick);
+}
+
+const handleDropdown = (e, section_container, state="toggle") => {
+  let dropdown_container = section_container.querySelector('.dropdown-nav');
+
+  const toggleMenu = () => dropdown_container.classList.toggle('hidden');
+  if (dropdown_container.classList.contains('hidden')) {
+    toggleMenu();
+    addOffClick(e, toggleMenu);
+  }
 }
